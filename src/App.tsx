9e966f8f14a130c332ad4c00702fa6a963dfe164
sunil barewar <sunilvbarewar@gmail.com>
2023-07-21 15:11:44 +0530

    🎉Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+  createBrowserRouter,
+  RouterProvider
+} from 'react-router-dom'
+import Home from './pages/Home';
+import Recipes from './pages/Recipes';
+import DetailPage from './pages/Detail';
+import SavedRecipes from './pages/Saved-Recipes';
 
 function App() {
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />
+    },
+    {
+      path: "/recipes",
+      element: <Recipes />
+    },
+    {
+      path: "/detail",
+      element: <DetailPage />
+    },
+    {
+      path: "/saved-recipes",
+      element: <SavedRecipes />
+    }
+  ])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RouterProvider router={router} />
   );
 }
 
