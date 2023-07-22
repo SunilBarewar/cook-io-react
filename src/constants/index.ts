b143744c17265bf9a49aa8c30e8da2937efffa02
sunil barewar <sunilvbarewar@gmail.com>
2023-07-22 15:17:07 +0530

    constants for home page

---
+++ src/constants/index.ts
@@ -0,0 +1,49 @@
+export const cardFieldsQueries = [
+    ["field", "uri"],
+    ["field", "label"],
+    ["field", "image"],
+    ["field", "totalTime"],
+]
+
+
+export const tabs = ["breakfast", "lunch", "dinner", "snack", "Teatime"];
+
+export const cuisineTypes = ["indian","asian", "french"];
+
+export const tags = [
+    'wheat-free',
+    'vegetarian',
+    'vegan',
+    'tree-nut-free',
+    'sulfite-free',
+    'sugar-conscious',
+    'soy-free',
+    'shellfish-free',
+    'sesame-free',
+    'red-meat-free',
+    'pork-free',
+    'pescatarian',
+    'peanut-free',
+    'paleo',
+    'No-oil-added',
+    'mustard-free',
+    'mollusk-free',
+    'Mediterranean',
+    'lupine-free',
+    'low-sugar',
+    'low-potassium',
+    'kosher',
+    'kidney-friendly',
+    'keto-friendly',
+    'immuno-supportive',
+    'gluten-free',
+    'fodmap-free',
+    'fish-free',
+    'egg-free',
+    'DASH',
+    'dairy-free',
+    'crustacean-free',
+    'celery-free',
+    'alcohol-free',
+    'alcohol-cocktail'
+  ];
